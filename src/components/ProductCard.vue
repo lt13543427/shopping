acfdd2976ffd41213a1375a82a59e81c62e1a5d1
vsCode git <152580878+lt13543427@users.noreply.github.com>
2024-03-04 13:10:35 +0800

    1

--- src/components/ProductCard.vue
+++ src/components/ProductCard.vue
@@ -1,15 +1,30 @@
 <script>
-
+export default{
+  data() {
+    return {
+      productdataArr: []
+    };
+  },
+  async mounted() {
+    try {
+      const product = await fetch('../MOCK_DATA.json');
+      const data = await product.json();
+      this.productdataArr = data;
+    } catch (error) {
+      console.error(error);
+    }
+  },
+}
 </script>
 
 <template>
-<div class="flex flex-wrap w-11/12 m-auto gap-4 justify-center">
-    <div class="w-1/6 min-w-[150px] min-h-[500px] flex flex-col rounded-xl border overflow-hidden ">
-      <img src="https://fakeimg.pl/300/">
+<div  class="flex flex-wrap w-11/12 m-auto gap-4 justify-center">
+    <div v-for="item in productdataArr" :key="item.id" class="w-1/6 min-w-[150px] min-h-[500px] flex flex-col rounded-xl border overflow-hidden ">
+      <img src="../img/下載.png" alt="">
       <div class="md-5">
-        <h2 class="my-4">Bytecard</h2>
-        <p class="my-4 break-words">Immunization not carried out because of caregiver refusal</p>
-        <p class="my-5">價格: 954</p>
+        <h2 class="my-4">{{item.name}}</h2>
+        <p class="my-4 break-words">{{ item.description }}</p>
+        <p class="my-5">{{ item.price }}</p>
       </div>
       <div class="flex-grow"></div>
       <div class="flex">
@@ -22,83 +37,5 @@
       </div>
       <div class="text-center bg-slate-500 text-white mt-3"><i class="fa-solid fa-cart-shopping relative"></i>加入購物車</div>
     </div>
-
-    <div class="w-1/6 min-w-[150px] min-h-[500px] flex flex-col rounded-xl border overflow-hidden ">
-      <img src="https://fakeimg.pl/300/">
-      <div class="md-5">
-        <h2 class="my-4">Bytecard</h2>
-        <p class="my-4 break-words">Immunization not carried out because of caregiver refusal</p>
-        <p class="my-5">價格: 954</p>
-      </div>
-      <div class="flex-grow"></div>
-      <div class="flex">
-        <div>數量：</div>
-        <div class="flex h-[35px]">
-          <button class="w-1/4 border rounded-l-xl">-</button>
-          <input type="number" class="w-2/4 border text-center" value="1">
-          <button class="w-1/4 border rounded-r-xl">+</button>
-        </div>
-      </div>
-      <div class="text-center bg-slate-500 text-white mt-3"><i class="fa-solid fa-cart-shopping relative"></i>加入購物車</div>
-    </div>
-
-    <div class="w-1/6 min-w-[150px] min-h-[500px] flex flex-col rounded-xl border overflow-hidden ">
-      <img src="https://fakeimg.pl/300/">
-      <div class="md-5">
-        <h2 class="my-4">Bytecard</h2>
-        <p class="my-4 break-words">Immunization not carried out because of caregiver refusal</p>
-        <p class="my-5">價格: 954</p>
-      </div>
-      <div class="flex-grow"></div>
-      <div class="flex">
-        <div>數量：</div>
-        <div class="flex h-[35px]">
-          <button class="w-1/4 border rounded-l-xl">-</button>
-          <input type="number" class="w-2/4 border text-center" value="1">
-          <button class="w-1/4 border rounded-r-xl">+</button>
-        </div>
-      </div>
-      <div class="text-center bg-slate-500 text-white mt-3"><i class="fa-solid fa-cart-shopping relative"></i>加入購物車</div>
-    </div>
-
-    <div class="w-1/6 min-w-[150px] min-h-[500px] flex flex-col rounded-xl border overflow-hidden ">
-      <img src="https://fakeimg.pl/300/">
-      <div class="md-5">
-        <h2 class="my-4">Bytecard</h2>
-        <p class="my-4 break-words">Immunization not carried out because of caregiver refusal</p>
-        <p class="my-5">價格: 954</p>
-      </div>
-      <div class="flex-grow"></div>
-      <div class="flex">
-        <div>數量：</div>
-        <div class="flex h-[35px]">
-          <button class="w-1/4 border rounded-l-xl">-</button>
-          <input type="number" class="w-2/4 border text-center" value="1">
-          <button class="w-1/4 border rounded-r-xl">+</button>
-        </div>
-      </div>
-      <div class="text-center bg-slate-500 text-white mt-3"><i class="fa-solid fa-cart-shopping relative"></i>加入購物車</div>
-    </div>
-
-    <div class="w-1/6 min-w-[150px] min-h-[500px] flex flex-col rounded-xl border overflow-hidden ">
-      <img src="https://fakeimg.pl/300/">
-      <div class="md-5">
-        <h2 class="my-4">Bytecard</h2>
-        <p class="my-4 break-words">Immunization not carried out because of caregiver refusal</p>
-        <p class="my-5">價格: 954</p>
-      </div>
-      <div class="flex-grow"></div>
-      <div class="flex">
-        <div>數量：</div>
-        <div class="flex h-[35px]">
-          <button class="w-1/4 border rounded-l-xl">-</button>
-          <input type="number" class="w-2/4 border text-center" value="1">
-          <button class="w-1/4 border rounded-r-xl">+</button>
-        </div>
-      </div>
-      <div class="text-center bg-slate-500 text-white mt-3"><i class="fa-solid fa-cart-shopping relative"></i>加入購物車</div>
-    </div>
-    
 </div>
-
 </template>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,23 +1,41 @@
 <script>
 import ProductCard from '@/components/ProductCard.vue';
-export default{
-  components:{
+import ProductCardList from '@/components/ProductCardList.vue';
+
+export default {
+  data() {
+    return {
+      currentView: 'table',
+      card: {},
+    };
+  },
+  methods: {
+    toggleView(view) {
+      this.currentView = view;
+    },
+  },
+  components: {
     ProductCard,
+    ProductCardList,
   },
 }
 </script>
+
 <template>
-  <div class="title-div mt-5 ml-3 mb-5 flex">
-    <h1 class="text-3xl font-bold">商品列表</h1>
-  </div>
-  <div class="flex gap-3 justify-end px-3">
-      <i class="fa-solid fa-table-cells-large"></i>
-      <i class="fa-solid fa-list"></i>
-  </div>
-  <div class="div">
-    <ProductCard></ProductCard>
+  <div>
+    <div class="title-div mt-5 ml-3 mb-5 flex">
+      <h1 class="text-3xl font-bold">商品列表</h1>
+    </div>
+    <div class="flex gap-3 justify-end px-3">
+      <i @click="toggleView('table')" class="fa-solid fa-table-cells-large"></i>
+      <i @click="toggleView('list')" class="fa-solid fa-list"></i>
+    </div>
+    <div class="div">
+      <ProductCard v-if="currentView === 'table'" v-model="card"></ProductCard>
+      <ProductCardList v-if="currentView === 'list'" v-model="card"></ProductCardList>
+    </div>
   </div>
 </template>
+
 <style scoped>
-  
 </style>
